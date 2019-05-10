795e079fd41ec2840633629ec7571969d45d60ab
Mahmut Bilgen <Mahmut.Bilgen@gmail.com>
2019-05-10 06:38:47 -0700

    initial

---
+++ main.py
@@ -0,0 +1,34 @@
+#!/usr/local/bin/python3
+
+import urllib
+from flask import Flask, jsonify
+from htmlparse.parser import HtmlParser
+
+app = Flask(__name__,static_url_path='')
+
+
+@app.route('/games/<path:gametitle>')
+def game(gametitle):
+    g = urllib.parse.unquote(gametitle)
+    r = HtmlParser().get_game(g)
+    return jsonify({'title': r[0], 'score': r[1]})
+
+
+@app.route('/games')
+def games_all():
+    r = HtmlParser().get_all()
+    return jsonify(r)
+
+
+@app.route('/')
+def index():
+    # return send_from_directory('','README.md')
+    # return Flask.render_template('filename.html')
+    return app.send_static_file('index.html')
+
+def main():
+    app.run(debug=True, port=8080)
+
+
+if __name__ == '__main__':
+    main()
