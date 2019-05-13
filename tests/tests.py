e894215eb54d90d7ad99810e9a08b9d35b4991c9
Mahmut Bilgen <Mahmut.Bilgen@gmail.com>
2019-05-13 16:12:13 -0700

    test.py changed

--- tests/tests.py
+++ tests/tests.py
@@ -8,7 +8,7 @@ import htmlparse.parser as p
 
 class test_metacritic_availability(unittest.TestCase):
     def setUp(self):
-        self.url = 'http://www.metacritic.com/game/playstation-4'
+        self.url = 'http://www.metacritic.com/game'
         self.headers = {
             'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36'}
 
